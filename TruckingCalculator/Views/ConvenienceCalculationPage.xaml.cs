e6fc51b5861172778381c1e5dc0b9a7a9c3747d6
John Thompson <Skalexein@gmail.com>
2018-11-04 19:21:59 -0500

    Updated calculation page
    Made it so that when the entry is focused the entry field is set to nothing.

--- TruckingCalculator/Views/ConvenienceCalculationPage.xaml.cs
+++ TruckingCalculator/Views/ConvenienceCalculationPage.xaml.cs
@@ -7,25 +7,51 @@ namespace TruckingCalculator.Views
 {
     public partial class ConvenienceCalculationPage : ContentPage
     {
-        public ConvenienceCalculationViewModel ViewModel
-        {
-            get;
-            set;
-        }
+        private ConvenienceCalculationViewModel viewModel = null;
         public ConvenienceCalculationPage()
         {
             InitializeComponent();
-            ViewModel = new ConvenienceCalculationViewModel
+            viewModel = new ConvenienceCalculationViewModel
             {
                 OriginalPayment = 0.0m,
                 FinalPayment = 0.0m,
                 PercentageOfMoneyWitheld = 0.0m
             };
+            BindingContext = this.viewModel;
+        }
+
+        void OriginalFocus(object sender, bool IsFocused)
+        {
+            
+            if (IsFocused)
+            {
+                _originalPayment.Text = "";
+            }
+        }
+
+        void FinalFocus(object sender, bool IsFocused)
+        {
+
+            if (IsFocused)
+            {
+                _finalPayment.Text = "";
+            }
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
         {
-            ViewModel.PercentageOfMoneyWitheld = 75.25m;
+            if (viewModel.OriginalPayment != 0)
+            {
+                var percentageOfPayment = viewModel.FinalPayment /
+                                         viewModel.OriginalPayment;
+
+                this.viewModel.PercentageOfMoneyWitheld =
+                    (1m - percentageOfPayment) * 100m;
+            }
+            else
+            {
+                DisplayAlert("Original payment should be greater than zero", "Original Payment cannot be zero.", "OK");
+            }
         }
     }
 }
